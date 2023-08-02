31e48421b47bca2677c92c836de12ce52d01174f
JD <juandiegogarita@gmail.com>
2023-08-01 21:54:35 -0600

    project init

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by JD on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
